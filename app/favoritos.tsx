59dce01993f2de85c90165520c28ace443a3b3e6
Eduardo Bianco <edubiapin@gmail.com>
2025-01-02 15:52:10 -0300

    criacao das paginas de favoritos historico e listas

---
+++ app/favoritos.tsx
@@ -0,0 +1,7 @@
+import { VStack } from "@/components/ui/vstack";
+
+export default function Favoritos() {
+    return (
+        <VStack></VStack>
+    )
+}
---
+++ app/historico.tsx
@@ -0,0 +1,7 @@
+import { VStack } from "@/components/ui/vstack";
+
+export default function Historico() {
+    return (
+        <VStack></VStack>
+    )
+}
