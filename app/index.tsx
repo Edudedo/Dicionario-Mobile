c51854801b8529df4f9b704ba2acfbb294b3e66b
Eduardo Bianco <edubiapin@gmail.com>
2025-01-02 17:24:50 -0300

    Troca de nomes e estilizacao do layout.tsx

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import Cabecalho from "@/components/ui/Cabecalho";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
+import { useToast } from "@/components/ui/toast";
+import { Link } from "expo-router";
+import {  ScrollView, View } from "react-native";
+
+
+
+
+export default function Lista() {
+
+  const toast = useToast
+ 
+  return (
+    
+    <ScrollView>
+    <Cabecalho></Cabecalho>
+    
+    </ScrollView>
+  );
+}
