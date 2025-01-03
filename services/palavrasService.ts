d8eff2c5760945ce419fd4dfdd7498a1939a760e
Eduardo Bianco <edubiapin@gmail.com>
2025-01-03 13:58:44 -0300

    serviço de pegar a definicao das palavras

--- services/palavrasService.ts
+++ services/palavrasService.ts
@@ -1,12 +1,23 @@
 import axios from "axios";
+import { Alert } from "react-native";
 
 export const getPalavras = async (palavra: string) => {
     try {
         const resposta = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${palavra}`)
-
+        
         return resposta.data
-    } catch (erro) {
-        console.error("Erro ao pegar definição da palavra:", erro)
+        
+    } catch (erro: any) {
+        if (erro.status === 404) {
+            Alert.alert(
+                "Erro na API", 
+                "Palavra não existente no banco de dados da Free Dictionary API", 
+                [
+                  { text: "OK", onPress: () => console.log("OK Pressed") },
+                ]
+              );
+        }
+        console.error("Erro ao pegar definição da palavra:", erro.status)
     }
 } 
 
